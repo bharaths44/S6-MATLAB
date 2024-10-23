ba4639b5da8670173356ce10763ffcb69fd5c07e
bharaths44 <bharathsatheesan@gmail.com>
2024-10-23 11:30:23 +0530

    rearanged codes according to lab cycle

--- Exp06/circle_area.m
+++
@@ -1,7 +0,0 @@
-radius = input('Enter the radius of the circle: ');
-if radius < 0
-    fprintf('Error: Radius cannot be negative.\n');
-else
-    area = circle_area(radius);
-    fprintf('The area of the circle with radius %.2f is: %.2f\n', radius, area);
-end
